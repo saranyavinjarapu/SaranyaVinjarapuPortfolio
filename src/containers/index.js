46f8091741b05e401f905d52f244f8fb4fdf4ecd
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-01-25 12:48:05 +0530

    created the layout of home page with all the necessary sections and relevent files and folders

---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+export { default as About } from "./About/About";
+export { default as Footer } from "./Footer/Footer";
+export { default as HeroSection } from "./HeroSection/HeroSection";
+export { default as Home } from "./Home/Home";
+export { default as Portfolio } from "./Portfolio/Portfolio";
+export { default as Skills } from "./Skills/Skills";
+export { default as Testimonials } from "./Testimonials/Testimonials";
