c6446f03bcc887a2799c3194101b5d3d7f842772
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-02-15 12:43:51 +0530

    added grid container for portfolio projects

---
+++ src/data/portfolioData.js
@@ -0,0 +1,34 @@
+export const portfolioData = [
+  {
+    id: "1",
+    src: require("../assets/selfocus_pf.png"),
+    title: "Chrome Extension",
+    description:
+      "Selfocus, a chrome extension based on pomodoro technique that helps users achieve productivity by eliminating distractions from their browser.",
+    link: "https://github.com/saranyavinjarapu/Selfocus_Landing_Page",
+  },
+  {
+    id: "2",
+    src: require("../assets/saveuser_pf.png"),
+    title: "Save User Application Dashboard",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
+    link: "View In Github",
+  },
+  {
+    id: "3",
+    src: require("../assets/tokenswapping_pf.png"),
+    title: "Token Swapping Interface",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
+    link: "View In Github",
+  },
+  {
+    id: "",
+    src: require("../assets/custom_tictactoe_pf.png"),
+    title: "Customizable Tic Tac Toe",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspenmdisse varius eim in eros elementum tristique",
+    link: "View In Github",
+  },
+];
