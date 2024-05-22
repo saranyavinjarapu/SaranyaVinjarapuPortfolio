887fc6f5956728cbf7e03692a0e80702cba46bfa
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-05-22 10:40:39 +0530

    added new project info to portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -29,6 +29,16 @@ export const portfolioData = [
   },
   {
     id: "4",
+    src: require("../assets/collection_crud_pf.png"),
+    title: "Collection CRUD",
+    description:
+      "Collection List Display with CRUD operations to enable users to add and delete collections ",
+    viewLink: "https://collection-crud.netlify.app/",
+    sourceLink: "https://github.com/saranyavinjarapu/CollectionCRUD",
+  },
+
+  {
+    id: "5",
     src: require("../assets/gpu_datavisualisation_pf.png"),
     title: "GPU Scattered Data Modeling",
     description:
@@ -40,7 +50,7 @@ export const portfolioData = [
       "https://etd.ohiolink.edu/acprod/odb_etd/ws/send_file/send?accession=akron1335297259&disposition=inline",
   },
   {
-    id: "5",
+    id: "6",
     src: require("../assets/autofluencer.png"),
     title: "AutoFluencer",
     description:
@@ -52,7 +62,7 @@ export const portfolioData = [
       "https://etd.ohiolink.edu/acprod/odb_etd/ws/send_file/send?accession=akron1335297259&disposition=inline",
   },
   {
-    id: "6",
+    id: "7",
     src: require("../assets/custom_tictactoe_pf.png"),
     title: "Customizable Tic Tac Toe",
     description:
